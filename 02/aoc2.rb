4fba478d5e074a03ca993111e07e1eb9f6ce030b
Benjamin Ortega <ben.ortega@gmail.com>
2022-12-05 10:03:04 -0600

    missed some stuff from day 2

--- 02/aoc2.rb
+++ 02/aoc2.rb
@@ -1,4 +1,44 @@
 class Game
+    attr_accessor :me, :opponent
+    MOVES = %i{paper rock scissors} 
+    ME_TRANSLATION = {X: :rock, Y: :paper, Z: :scissors}
+    OPPONENT_TRANSLATION = {A: :rock, B: :paper, C: :scissors}
+   
+    def initialize(me:, opponent:)
+        @me=ME_TRANSLATION[me.intern] 
+        @opponent=OPPONENT_TRANSLATION[opponent.intern]
+        
+    end
+
+    # 0=tie
+    def me_outcome_score
+        # -1 or +2 is win
+        # +1 or -2 is loss 
+        return case MOVES.index(me) - MOVES.index(opponent)
+        when 2, -1
+            6
+        when 1, -2
+            0
+        else
+            3
+        end
+    end
+
+    def me_choice_score
+        %i{rock paper scissors} .index(me)+1
+    end
+
+    def me_score
+        me_choice_score + me_outcome_score
+    end
+
+    def to_s
+        "ME: #{me}\tOPP: #{opponent}\t#{me_choice_score}\t#{me_outcome_score}=#{me_score}"
+    end
+
+end
+
+class DesiredGame < Game
     attr_accessor :me, :opponent, :outcome
     MOVES = %i{paper rock scissors} 
     OPPONENT_TRANSLATION = {A: :rock, B: :paper, C: :scissors}
