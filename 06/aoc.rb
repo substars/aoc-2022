15f1ebf16010dad29de9a6efdf45bf556a6731fe
Benjamin Ortega <ben.ortega@gmail.com>
2022-12-05 23:23:03 -0600

    DRY up day 6

--- 06/aoc.rb
+++ 06/aoc.rb
@@ -1,21 +1,22 @@
-lines = File.read('input.txt').split("\n")
+lines = File.read('example.txt').split("\n")
+
+def get_start_of_seq(line, num_chars)
+    line.each_char.with_index.inject([]) do |last_few, (char, i)|
+        if i <= num_chars-1
+            last_few << char
+        else
+            last_few.push(char)
+            last_few.shift
+        end    
+        if last_few.uniq.length == num_chars
+            return i+1
+        end
+        last_few
+    end    
+end
 
 lines.each do |line|
-    #puts "processing #{line}"
-    last_four = [] 
-    line.each_char.with_index do |char, i|
-        if i <= 3
-            last_four << char
-        else
-            last_four.push(char)
-            last_four.shift
-        end         
-        if last_four.uniq.length == 4
-            puts "line #{line} has SoP # #{i+1}"
-            break
-        end
-    end
-    puts
+    puts "line #{line} has SoP # #{get_start_of_seq(line, 4)}"
 end
 
 puts
@@ -23,19 +24,5 @@ puts "PART 2"
 puts
 
 lines.each do |line|
-    #puts "processing #{line}"
-    last_fourteen = [] 
-    line.each_char.with_index do |char, i|
-        if i <= 13
-            last_fourteen << char
-        else
-            last_fourteen.push(char)
-            last_fourteen.shift
-        end         
-        if last_fourteen.uniq.length == 14
-            puts "line #{line} has SoM # #{i+1}"
-            break
-        end
-    end
-    puts
+    puts "line #{line} has SoM # #{get_start_of_seq(line, 14)}"
 end
