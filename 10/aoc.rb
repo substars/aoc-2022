935276a0c681310b694beee7aac3d26aa952370d
Benjamin Ortega <ben.ortega@gmail.com>
2022-12-10 10:29:45 -0600

    DRY some stuff, don't need the buffer

--- 10/aoc.rb
+++ 10/aoc.rb
@@ -1,50 +1,47 @@
 class CPU
-  attr_accessor :x, :cycle, :buffer
+  attr_accessor :x, :cycle
 
   def initialize
     self.x = 1
     self.cycle = 0
-    self.buffer = ""
   end
 
-  def noop(_)
-    cycle!
-    yield x, cycle if block_given?
+  def noop(_ = nil, &block)
+    cycle!(&block)
   end
 
-  def addx(val)
-    cycle!
-    yield(x, cycle) if block_given?
-    cycle!
-    yield(x, cycle) if block_given?
-    self.x += val
+  def addx(val = nil, &block)
+    2.times { cycle!(&block) }
+    self.x += val.to_i
   end
 
+  # also draws stuff
   def cycle!
-    self.buffer << if ((x - 1)..(x + 1)).include?(cycle % 40)
-      "#"
-    else
-      "."
-    end
+    pixel = if ((x - 1)..(x + 1)).include?(cycle % 40)
+        "#"
+      else
+        "."
+      end
+    print pixel
     self.cycle += 1
+    print "\n" if cycle % 40 == 0
+    yield(self) if block_given?
   end
 
-  def draw!
-    retval = self.buffer
-    self.buffer = ""
-    return retval
+  def signal_strength
+    if cycle % 40 == 20
+      cycle * x
+    else
+      0
+    end
   end
 end
 
 c = CPU.new
 signal_strengths_total = 0
 File.read("input.txt").split("\n").each do |line|
-  instruction, val = line.split(" ")
-  c.send(instruction, val.to_i) do |x, cycle|
-    if [20, 60, 100, 140, 180, 220].include?(cycle)
-      #puts "signal strength at #{cycle} is #{cycle * x} (#{x})"
-      signal_strengths_total += (cycle * x)
-    end
+  c.send(*line.split(" ")) do |cpu|
+    signal_strengths_total += cpu.signal_strength
   end
 end
 
@@ -56,10 +53,5 @@ puts
 c = CPU.new
 
 File.read("input.txt").split("\n").each do |line|
-  instruction, val = line.split(" ")
-  c.send(instruction, val.to_i) do |x, cycle|
-    if cycle % 40 == 0
-      puts c.draw!
-    end
-  end
+  c.send(*line.split(" "))
 end
