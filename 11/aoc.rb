5576c09fb08cc7fd67c88b6eca93284ba39eab87
Benjamin Ortega <ben.ortega@gmail.com>
2022-12-11 00:32:44 -0600

    day 11 part 2

--- 11/aoc.rb
+++ 11/aoc.rb
@@ -1,10 +1,10 @@
 monkey_chunks = File.read("input.txt").split("\n\n")
 
 class Monkey
-  attr_accessor :items, :name, :inspected
+  attr_accessor :items, :name, :inspected, :test_divisor
 
   def initialize(chunk)
-    @inspected=0
+    @inspected = 0
     @bits = chunk.split("\n")
     @name = @bits.first
     @items = @bits[1].scan(/\d+/).map(&:to_i)
@@ -14,16 +14,20 @@ class Monkey
 
   def inspect_items
     @items.map do |old|
-      @inspected+=1
-      updated = (eval(@operation) / 3).to_i
+      @inspected += 1
+      updated = operation(old)
       to_monkey = if updated % @test_divisor == 0
         @bits[4]
       else
         @bits[5]
       end.scan(/\d+/).first.to_i
-      yield updated, to_monkey if block_given?      
+      yield updated, to_monkey if block_given?
     end
     @items.clear
+  end
+
+  def operation(old)
+    (eval(@operation) / 3).to_i
   end
 
   def to_s
@@ -34,21 +38,42 @@ class Monkey
   end
 end
 
-monkeys = monkey_chunks.map do |chunk|
-  Monkey.new(chunk)
-end
+class BigMonkey < Monkey # for part 2
+  attr_accessor :modulo
 
-20.times do |i|
-  monkeys.each do |monkey|   
-     monkey.inspect_items do |item, new_monkey|
-       puts "moving item #{item} from #{monkey.name} to #{new_monkey}"
-       monkeys[new_monkey].items << item
-     end
+  def initialize(chunk)
+    super(chunk)
+    @modulo = 1
   end
-  puts "** ROUND #{i+1}"
-  monkeys.each_with_index do |m, i|
-    puts "#{m.name}: #{m.items.join(", ")} #{m.inspected}"
-end
+
+  def operation(old)
+    (eval(@operation) % modulo).to_i
+  end
 end
 
-puts monkeys.map(&:inspected).sort.last(2).inject(&:*)
+def sling_stuff(monkeys, rounds)
+  rounds.times do |i|
+    monkeys.each do |monkey|
+      monkey.inspect_items do |item, new_monkey|
+        monkeys[new_monkey].items << item
+      end
+    end
+  end
+end
+
+monkeys = monkey_chunks.map { |chunk| Monkey.new(chunk) }
+sling_stuff(monkeys, 20)
+puts "Part 1 solution: #{monkeys.map(&:inspected).sort.last(2).inject(&:*)}"
+
+puts "### PART 2 ###"
+
+modulo = 1
+monkeys = monkey_chunks.map do |chunk|
+  BigMonkey.new(chunk).tap do |m|
+    modulo *= m.test_divisor
+  end
+end
+
+monkeys.each { |m| m.modulo = modulo }
+sling_stuff(monkeys, 10_000)
+puts "Part 2 solution: #{monkeys.map(&:inspected).sort.last(2).inject(&:*)}"
